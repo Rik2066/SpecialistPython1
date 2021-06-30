3a33e7f6cda2e528c5a8e4ba72a92cff00028ba8
Rik2066 <86655228+Rik2066@users.noreply.github.com>
2021-06-30 17:07:12 +0300

    Update 01_task_dict.py

--- Module3/practice/dict/01_task_dict.py
+++ Module3/practice/dict/01_task_dict.py
@@ -4,7 +4,10 @@
 # Считая,что курс доллара равен dollar_rate
 # Вычислите цену всех товаров с названием "name" в долларах
 
+
 item = {"name": "Кроссовки", "price": "7540.5", "currency": "rub", "count": "10"}
 dollar_rate = 74.12
-
-# TODO: your code here
+cost = item["price"]
+n = item["count"]
+sell = float(cost) * float(n) / dollar_rate
+print("Цена кроссовок:", round(sell,1))
